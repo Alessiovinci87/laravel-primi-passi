a8db62ae2af628f35b4b44f79958298a80353591
Alessiovinci87 <alessio.vinci87@gmail.com>
2023-09-07 21:41:45 +0200

    Bonus terminato

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,1 @@
+<h1>TEST CONTACT</h1>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,40 +1,58 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel Primi Passi</title>
+    <title>Laravel Primi Passi</title>
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
 
-        <!-- Styles -->
-        <style>
-            body {
+    <!-- Styles -->
+    <style>
+        body {
             background-color: lightcoral;
             display: flex;
-            justify-content: center;
+            flex-direction: column;
             align-items: center;
+            justify-content: center;
             height: 100vh;
-            }
+            margin: 0;
+        }
 
-            .hello-world {
-            color: YELLOW;
+        .hello-world {
+            color: yellow;
             font-size: 36px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="hello-world">
-            HELLO WORLD
-        </div>    
-        {{-- <nav>
-            <ul>
-                <li><a href="/">Contact</a></li>
-                <li><a href="/about">About</a></li>
-            </ul>
-        </nav> --}} 
-    </body>
+            margin-bottom: 20px;
+            font-size: 48px;
+        }
+
+        nav ul {
+            list-style-type: none;
+            padding: 0;
+            margin: 0;
+            text-align: center;
+            font-size: 30px;
+        }
+
+        nav li {
+            display: inline-block;
+            margin: 0 10px;
+        }
+    </style>
+</head>
+<body>
+    <div class="hello-world">
+        HELLO WORLD
+    </div>
+
+    <nav>
+        <ul>
+            <li><a href="{{ route('contact') }}">Contact</a></li>
+            <li><a href="{{ route('about') }}">About</a></li>
+        </ul>
+    </nav>
+</body>
 </html>
